19ae1f99fb34b31512aa5f9118bf7670325d6217
tysonlander <tysonlander@gmail.com>
2022-03-22 21:20:47 -0600

    Added module alias and displayed events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,28 @@
-import Layout from '../../components/Layout';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
 
-export default function EventsPage() {
-  return <Layout>My Events page</Layout>;
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No Events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  // getServerSideProps is going to run every time we come to the page
+  // getStaticProps. only runs once at build time
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1, // every one second it will revalidate if the data has changed
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  // getServerSideProps is going to run every time we come to the page
+  // getStaticProps. only runs once at build time
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1, // every one second it will revalidate if the data has changed
+  };
 }
